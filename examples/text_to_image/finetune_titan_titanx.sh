bdbb7b633904b3edc36571e1b1528e6a41820473
Anton <antonwiehe@gmail.com>
2022-12-02 12:33:52 +0000

    less res

--- examples/text_to_image/finetune_titan_titanx.sh
+++ examples/text_to_image/finetune_titan_titanx.sh
@@ -1,18 +1,18 @@
-accelerate launch --mixed_precision="bf16" train_text_to_image.py \
-  --pretrained_model_name_or_path="../../../mus2vid/models/stable-diffusion-v1-5" \
-  --train_data_dir_var_aspect="/hdd/data/finetune_SD/laion_aesthetics"  \
+accelerate launch --mixed_precision="fp16" --num_processes=2 train_text_to_image.py \
+  --pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5" \
+  --train_data_dir_var_aspect="../../../finetune_SD/laion_aesthetics_2/"  \
   --train_batch_size=1 \
   --gradient_accumulation_steps=8 \
   --gradient_checkpointing \
   --max_train_steps=2000 \
-  --learning_rate=8e-07 \
+  --learning_rate=2e-06 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=100 \
-  --output_dir="sdv1-5-var-aspect-3" \
+  --output_dir="sdv1-5-var-aspect-1" \
   --use_8bit_adam \
-  --mixed_precision="bf16" \
+  --mixed_precision="fp16" \
   --max_files=100 \
-  --max_width=768 \
-  --max_height=768 \
+  --max_width=256 \
+  --max_height=256 \
 
 #  --use_ema \
